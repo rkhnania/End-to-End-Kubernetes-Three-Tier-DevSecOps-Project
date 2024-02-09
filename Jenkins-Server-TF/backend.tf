224ad11dfa826db1db6b45f15ccd84542feaa988
Ramsin Khnania <rkhnania@gmail.com>
2024-02-09 19:27:21 +1100

    frist commit

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "my-terraform-state-bucket"
+    region         = "ap-southeast-2"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "my-Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
